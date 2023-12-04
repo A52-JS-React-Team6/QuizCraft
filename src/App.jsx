1862ac20160c5bb12fb4727bee2fd38e4683ae74
Georgi Ivanov <georgi.ivanov.a52@learn.telerikacademy.com>
2023-12-04 22:17:52 +0200

    Undo my last changes.

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ import  { QuizResults } from './components/QuizResults/QuizResults';
 import { NavBarNew } from './components/NavBarNew/NavBar';
 import { NavBar } from './components/NavBar/NavBar'
 import { EditProfile } from './components/EditProfile/EditProfile';
-import { AllQuizzes } from './components/AllQuizzes/AllQuizzes';
+import { ManageQuizzes } from './components/ManageQuizzes/ManageQuizzes'
 import { ApiResponse } from './components/ApiResponse/ApiResponse';
 import { AdminView } from './views/Admin/Admin';
 import { StudentDashboard } from './components/StudentDashboard/StudentDashboard';
@@ -50,7 +50,7 @@ function App() {
           {/* <Route path="/edit-profile" element={<EditProfileView />} />
           <Route path="/manage-users" element={<ManageUsers />} /> */}
           <Route path='/edit-profile' element={<EditProfile />} />
-          <Route path="/all-quizzes" element={<AllQuizzes />} />
+          <Route path="/manage-quizzes" element={<ManageQuizzes />} />
           <Route path="/admin-page" element={<AdminView />} />
           <Route path="*" element={<Home />} />
           <Route path="/student-dashboard" element={<StudentDashboard />} />
